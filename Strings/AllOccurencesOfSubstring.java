f2df423297007a7258d6f84747b19f01c88715b5
Aryan Maan <115601348+aryann2319@users.noreply.github.com>
2025-06-27 09:34:24 +0530

    Update AllOccurencesOfSubstring.java

--- Strings/AllOccurencesOfSubstring.java
+++ Strings/AllOccurencesOfSubstring.java
@@ -8,5 +8,25 @@ public class AllOccurencesOfSubstring {
             System.out.println("Present at index: " + index);
             index = str.indexOf(substr, index + 1);
         }
+
+        // using for loop
+        String str = "ababcabcab";
+        String substr = "abc";
+
+        for (int i = 0; i <= str.length() - substr.length(); i++) {
+            boolean match = true;
+
+            for (int j = 0; j < substr.length(); j++) {
+                if (str.charAt(i + j) != substr.charAt(j)) {
+                    match = false;
+                    break;
+                }
+            }
+
+            if (match) {
+                System.out.println("Found at index: " + i);
+                i=i+j;
+            }
+        }
     }
 }
